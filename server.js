8b210038002b6f97707eaadf93f747afa57fb578
David-Sparky <sparkd@rpi.edu>
2017-09-09 13:01:14 -0400

    test bot

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var app = express();
 var http = require('http')
+var info = require('./var.js');
+var request = require('request')
+var slack = require('express-slack');
 
+// the path for OAuth, slash commands, and event callbacks
+app.use('/slack', slack({
+  scope: info.scope,
+  token: info.token,
+  store: 'data.json',
+  client_id: info.client_id,
+  client_secret: info.client_secret
+}));
 
+// handle the "/test" slash commands
+slack.on('/test', (payload, bot) => {
+  bot.reply('works!');
+});
 
 app.post('/slack_responding', function (req, res) {
-    res.send("RESP");
+  var token = info.token;
 });
 
 app.post('/not_responding', function (req, res) {
