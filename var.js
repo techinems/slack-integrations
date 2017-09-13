c7ca2b9162ce1187f4cd6c82514d1624c75c39ad
Dan Bruce <ddbruce@users.noreply.github.com>
2017-09-13 16:49:19 -0400

    Create var.js

---
+++ var.js
@@ -0,0 +1,7 @@
+module.exports = {
+  token: "XOXB_SLACK-TOKEN-HERE",
+  scope: "channels:write",
+  client_id: "CLIENT-ID-HERE",
+  client_secret: "CLIENT-SECRET-HERE",
+  verification_email: "BEING-USED-FOR-OTHER-THINGS"
+}
