271aa2969e67b83011c72a14f52b2566cd125e84
Dan Bruce <dan@ddbruce.com>
2017-09-18 14:58:04 -0400

    removed var.js from repo; fixed timestamp

--- var.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  token: "XOXB_SLACK-TOKEN-HERE",
-  scope: "channels:write",
-  client_id: "CLIENT-ID-HERE",
-  client_secret: "CLIENT-SECRET-HERE",
-  verification_email: "BEING-USED-FOR-OTHER-THINGS"
-}
