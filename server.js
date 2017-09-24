3ea77fa41ef13eb8de206e97d40d9cc33b74eaf8
Dan Bruce <dan@ddbruce.com>
2017-09-23 20:07:16 -0400

    more testing code; removed explicit testing script

--- server.js
+++ server.js
@@ -7,8 +7,11 @@ var slack = require('express-slack');
 var parser = require('xml2json');
 const bodyParser = require('body-parser');
 
+var slack_channel = 'C71B0PRDW';
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 app.use('/slack', slack({
   scope: info.scope,
@@ -24,33 +27,21 @@ app.post('/whoson', function(req, res) {
   });
 });
 
-function makeDate(clean = false) {
+function makeDate() {
   var now = new Date();
-
-  if (clean) {
-    return [
-      now.getFullYear(),
-      now.getMonth() + 1 < 10 ? "0" + (now.getMonth() + 1) : (now.getMonth() + 1),
-      now.getDate() + 1 < 10 ? "0" + (now.getDate() + 1) : (now.getDate() + 1),
-      now.getHours() < 10 ? "0" + (now.getHours()) : (now.getHours()),
-      now.getMinutes() < 10 ? "0" + (now.getMinutes()) : (now.getMinutes()),
-      now.getSeconds() < 10 ? "0" + (now.getSeconds()) : (now.getSeconds())
-    ].join('');
-  } else {
-    return [
-      now.getFullYear(),
-      '-',
-      now.getMonth() + 1 < 10 ? "0" + (now.getMonth() + 1) : (now.getMonth() + 1),
-      '-',
-      now.getDate() + 1 < 10 ? "0" + (now.getDate() + 1) : (now.getDate() + 1),
-      ' at ',
-      now.getHours() < 10 ? "0" + (now.getHours()) : (now.getHours()),
-      ':',
-      now.getMinutes() < 10 ? "0" + (now.getMinutes()) : (now.getMinutes()),
-      ':',
-      now.getSeconds() < 10 ? "0" + (now.getSeconds()) : (now.getSeconds())
-    ].join('');
-  }
+  return [
+    now.getFullYear(),
+    '-',
+    now.getMonth() + 1 < 10 ? "0" + (now.getMonth() + 1) : (now.getMonth() + 1),
+    '-',
+    now.getDate() + 1 < 10 ? "0" + (now.getDate() + 1) : (now.getDate() + 1),
+    ' at ',
+    now.getHours() < 10 ? "0" + (now.getHours()) : (now.getHours()),
+    ':',
+    now.getMinutes() < 10 ? "0" + (now.getMinutes()) : (now.getMinutes()),
+    ':',
+    now.getSeconds() < 10 ? "0" + (now.getSeconds()) : (now.getSeconds())
+  ].join('');
 }
 
 app.post('/tmd_slack_notification', function(req, res) {
@@ -59,7 +50,7 @@ app.post('/tmd_slack_notification', function(req, res) {
 
     var message = {
       unfurl_links: true,
-      channel: 'G6XGMATUP',
+      channel: slack_channel,
       token: info.token,
       "attachments": [
         {
@@ -81,13 +72,13 @@ app.post('/tmd_slack_notification', function(req, res) {
               "text": "Yes",
               "style": "danger",
               "type": "button",
-              "value": "yes_" + makeDate(true)
+              "value": "yes"
             },
             {
               "name": "status",
               "text": "No",
               "type": "button",
-              "value": "no_" + makeDate(true)
+              "value": "no"
             }
           ]
         }
@@ -107,47 +98,57 @@ app.post('/tmd_slack_notification', function(req, res) {
 app.post("/slack_response", function(req, res) {
   var strReq = req.body.payload.toString();
   var strReq = JSON.parse(strReq);
-  var strReqarr = strReq.actions[0].value.split("_");
 
-  var responding = strReqarr[0];
-  var ts = strReqarr[1];
+  var messageTime = new Date(strReq.message_ts);
+  var actionTime = new Date(strReq.action_ts);
+  var maxElapsedTime = 12; //minutes to allow responses
 
-  usernameUppercase = strReq.user.name.charAt(0).toUpperCase() + strReq.user.name.slice(1);
-  if (responding == "yes") {
-    console.log(usernameUppercase + " replied yes");
-    var response_message = {
-      unfurl_links: true,
-      channel: 'G6XGMATUP',
-      token: info.token,
-      "mrkdwn": true,
-      "attachments": [
-        {
-          "fallback": usernameUppercase + " is RESPONDING",
-          "text": "*" + usernameUppercase + "*" + " is *RESPONDING*",
-          "color": "#7CD197",
-          "mrkdwn_in": ["text"]
-        }
-      ]
+  // if (actionTime < messageTime + maxElapsedTime * 60) {
+  if (false) {
+    usernameUppercase = strReq.user.name.charAt(0).toUpperCase() + strReq.user.name.slice(1);
+    if (responding == "yes") {
+      console.log(usernameUppercase + " replied yes");
+      var response_message = {
+        unfurl_links: true,
+        channel: slack_channel,
+        token: info.token,
+        "mrkdwn": true,
+        "attachments": [
+          {
+            "fallback": usernameUppercase + " is RESPONDING",
+            "text": "*" + usernameUppercase + "*" + " is *RESPONDING*",
+            "color": "#7CD197",
+            "mrkdwn_in": ["text"]
+          }
+        ]
+      }
+    } else {
+      console.log(usernameUppercase + " replied no");
+      var response_message = {
+        unfurl_links: true,
+        channel: slack_channel,
+        token: info.token,
+        "mrkdwn": true,
+        "attachments": [
+          {
+            "fallback": usernameUppercase + " is not responding",
+            "text": usernameUppercase + " is NOT RESPONDING"
+          }
+        ]
+      }
     }
+    res.status(200).send();
+    slack.send('chat.postMessage', response_message);
   } else {
-    console.log(usernameUppercase + " replied no");
     var response_message = {
       unfurl_links: true,
-      channel: 'G6XGMATUP',
+      channel: slack_channel,
       token: info.token,
-      "mrkdwn": true,
-      "attachments": [
-        {
-          "fallback": usernameUppercase + " is not responding",
-          "text": usernameUppercase + " is NOT RESPONDING"
-        }
-      ]
+      "text": "Sorry, your response was logged too long after the initial dispatch went out."
     }
+    slack.send('chat.postEphemeral', )
   }
 
-  res.status(200).send();
-
-  slack.send('chat.postMessage', response_message);
 });
 
 app.listen(5939, function () {
