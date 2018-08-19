d8547e4922c7287a079b92664a267cc9a818ea1c
Dan Bruce <dan@ddbruce.com>
2018-08-19 12:08:33 -0400

    add dispatch channel to supliment AiR

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ var slack = require('express-slack');
 var parser = require('xml2json');
 const bodyParser = require('body-parser');
 
-// var slack_channel = 'C71B0PRDW'; //#development_scratch
-var slack_channel = 'G6XGMATUP'; //#responding
+// var air_channel = 'C71B0PRDW'; //#development_scratch
+var air_channel = 'G6XGMATUP'; //#responding
+var dispatch_channel = 'GAG3D0EBF'; //#dispatch
+var rpialert_channel = 'C6WT63HM3';
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -35,81 +37,81 @@ app.post('/whoson', function(req, res) {
     switch (req.body.text.toLowerCase()) {
       case "yest":
       case "yesterday":
-        o.setDate(d.getDate() - 1);
-        var date = makeWhosonDate(o) + "&yesterday=1";
-        break;
+      o.setDate(d.getDate() - 1);
+      var date = makeWhosonDate(o) + "&yesterday=1";
+      break;
 
       case "tom":
       case "tomorrow":
-        o.setDate(d.getDate() + 1);
-        var date = makeWhosonDate(o) + "&tomorrow=1";
-        break;
+      o.setDate(d.getDate() + 1);
+      var date = makeWhosonDate(o) + "&tomorrow=1";
+      break;
 
       case "sun":
       case "sunday":
-        o.setDate(d.getDate() + (7 - d.getDay()));
-        var date = makeWhosonDate(o);
-        break;
+      o.setDate(d.getDate() + (7 - d.getDay()));
+      var date = makeWhosonDate(o);
+      break;
 
       case "mon":
       case "monday":
-        if (d.getDay() < 1) {
-          o.setDate(d.getDate() + (1 - d.getDay()));
-        } else {
-          o.setDate(d.getDate() + (7 - (d.getDay() - 1)));
-        }
-        var date = makeWhosonDate(o);
-        break;
+      if (d.getDay() < 1) {
+        o.setDate(d.getDate() + (1 - d.getDay()));
+      } else {
+        o.setDate(d.getDate() + (7 - (d.getDay() - 1)));
+      }
+      var date = makeWhosonDate(o);
+      break;
 
       case "tues":
       case "tuesday":
-        if (d.getDay() < 2) {
-          o.setDate(d.getDate() + (2 - d.getDay()));
-        } else {
-          o.setDate(d.getDate() + (7 - (d.getDay() - 2)));
-        }
-        var date = makeWhosonDate(o);
-        break;
+      if (d.getDay() < 2) {
+        o.setDate(d.getDate() + (2 - d.getDay()));
+      } else {
+        o.setDate(d.getDate() + (7 - (d.getDay() - 2)));
+      }
+      var date = makeWhosonDate(o);
+      break;
 
       case "wed":
       case "wednesday":
-        if (d.getDay() < 3) {
-          o.setDate(d.getDate() + (3 - d.getDay()));
-        } else {
-          o.setDate(d.getDate() + (7 - (d.getDay() - 3)));
-        }
-        var date = makeWhosonDate(o);
-        break;
+      if (d.getDay() < 3) {
+        o.setDate(d.getDate() + (3 - d.getDay()));
+      } else {
+        o.setDate(d.getDate() + (7 - (d.getDay() - 3)));
+      }
+      var date = makeWhosonDate(o);
+      break;
 
       case "thurs":
       case "thursday":
-        if (d.getDay() < 4) {
-          o.setDate(d.getDate() + (4 - d.getDay()));
-        } else {
-          o.setDate(d.getDate() + (7 - (d.getDay() - 4)));
-        }
-        var date = makeWhosonDate(o);
-        break;
+      if (d.getDay() < 4) {
+        o.setDate(d.getDate() + (4 - d.getDay()));
+      } else {
+        o.setDate(d.getDate() + (7 - (d.getDay() - 4)));
+      }
+      var date = makeWhosonDate(o);
+      break;
 
       case "fri":
       case "friday":
-        if (d.getDay() < 5) {
-          o.setDate(d.getDate() + (5 - d.getDay()));
-        } else {
-          o.setDate(d.getDate() + (7 - (d.getDay() - 5)));
-        }
-        var date = makeWhosonDate(o);
-        break;
+      if (d.getDay() < 5) {
+        o.setDate(d.getDate() + (5 - d.getDay()));
+      } else {
+        o.setDate(d.getDate() + (7 - (d.getDay() - 5)));
+      }
+      var date = makeWhosonDate(o);
+      break;
 
       case "sat":
       case "saturday":
-        o.setDate(d.getDate() + (6 - d.getDay()));
-        var date = makeWhosonDate(o);
-        break;
+      o.setDate(d.getDate() + (6 - d.getDay()));
+      var date = makeWhosonDate(o);
+      break;
 
       default:
-        res.status(200).send("Please enter a valid day parameter and try again.");
-        done = true;
+      res.status(200).send("Please enter a valid day parameter and try again.");
+      done = true;
     }
 
     if (!done) {
@@ -164,11 +166,13 @@ app.post('/tmd_slack_notification', function(req, res) {
 
   if (req.body.verification == info.verification_email) {
 
+    var air_message = "";
+    var dispatch_message = "";
+
     if (compareTime(05, 55, "gt") && compareTime(18, 05, "lt")) {
-      var message = "";
-      message = {
+      air_message = {
         unfurl_links: true,
-        channel: slack_channel,
+        channel: air_channel,
         token: info.token,
         "attachments": [
           {
@@ -202,10 +206,23 @@ app.post('/tmd_slack_notification', function(req, res) {
           }
         ]
       };
+      var dispatch_message =  {
+        unfurl_links: false,
+        channel: dispatch_channel,
+        token: info.token,
+        "text": "RPI Ambulance dispatched on " + makeDate(),
+        "fallback": req.body.dispatch,
+        "attachments": [
+          {
+            "title": req.body.dispatch,
+            "short": true
+          }
+        ]
+      };
     } else {
-      message = {
+      air_message = {
         unfurl_links: true,
-        channel: slack_channel,
+        channel: air_channel,
         token: info.token,
         "attachments": [
           {
@@ -224,8 +241,23 @@ app.post('/tmd_slack_notification', function(req, res) {
           }
         ]
       };
+      var dispatch_message =  {
+        unfurl_links: false,
+        channel: dispatch_channel,
+        token: info.token,
+        "text": "RPI Ambulance dispatched on " + makeDate(),
+        "fallback": req.body.dispatch,
+        "attachments": [
+          {
+            "title": req.body.dispatch,
+            "value": "Night crew call. No response is needed.",
+            "short": true
+          }
+        ]
+      };
     }
-    slack.send('chat.postMessage', message);
+    slack.send('chat.postMessage', air_message);
+    slack.send('chat.postMessage', dispatch_message);
     res.status(200).send(req.body.dispatch);
   } else {
     res.status(401).send();
@@ -236,9 +268,9 @@ app.post('/tmd_slack_notification_long', function(req, res) {
 
   if (req.body.verification == info.verification_email) {
 
-    var message = {
+    var air_message = {
       unfurl_links: true,
-      channel: slack_channel,
+      channel: air_channel,
       token: info.token,
       "attachments": [
         {
@@ -257,7 +289,29 @@ app.post('/tmd_slack_notification_long', function(req, res) {
         }
       ]
     };
-    slack.send('chat.postMessage', message);
+    var dispatch_message = {
+      unfurl_links: true,
+      channel: dispatch_channel,
+      token: info.token,
+      "attachments": [
+        {
+          "text": "Rensslaer County longtone on " + makeDate(),
+          "fallback": req.body.dispatch,
+          "callback_id": "responding",
+          "color": "#F35A00",
+          "attachment_type": "default",
+          "fields": [
+            {
+              "title": req.body.dispatch,
+              "value": "",
+              "short": true
+            }
+          ]
+        }
+      ]
+    };
+    slack.send('chat.postMessage', air_message);
+    slack.send('chat.postMessage', dispatch_message);
     res.status(200).send(req.body.dispatch);
   } else {
     res.status(401).send();
@@ -288,7 +342,7 @@ app.post("/slack_response", function(req, res) {
         console.log(abbrname + " replied yes");
         response_message = {
           unfurl_links: true,
-          channel: slack_channel,
+          channel: air_channel,
           token: info.token,
           "mrkdwn": true,
           "attachments": [
@@ -304,7 +358,7 @@ app.post("/slack_response", function(req, res) {
         console.log(abbrname + " replied no");
         response_message = {
           unfurl_links: true,
-          channel: slack_channel,
+          channel: air_channel,
           token: info.token,
           "mrkdwn": true,
           "attachments": [
@@ -321,7 +375,7 @@ app.post("/slack_response", function(req, res) {
 
   } else {
     var response_message = {
-      channel: slack_channel,
+      channel: air_channel,
       token: info.token,
       user: userID,
       as_user: true,
@@ -342,6 +396,8 @@ app.use(express.static('.'));
 text = "";
 oldtext = "";
 
+var rpialertvar = require("//alert.rpi.edu/alerts.js");
+
 function rpialert() {
   request("http://alert.rpi.edu/data/alert/alerts.xml", function(error, response, body) {
     var json = JSON.parse(parser.toJson(body));
@@ -355,7 +411,7 @@ function rpialert() {
       if (oldtext != text) {
         var message =  {
           unfurl_links: false,
-          channel: 'C6WT63HM3',
+          channel: dispatch_channel,
           token: info.token,
           "text": "RPI ALERT - <!channel>",
           "fallback": "RPI ALERT: " + text,
